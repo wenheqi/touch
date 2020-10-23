654ed4ac407c91ee8522833dce19edc573f27450
wenheqi <wenhe.qi@outlook.com>
2020-10-22 17:51:21 -0700

    Update touch.js

--- touch.js
+++ touch.js
@@ -2,21 +2,29 @@
 const dut = document.getElementById("testdiv");
 
 dut.ontouchstart = function(evt) {
-  evt.preventDefault();
+  evt.preventDefault(); // prevent screen scrolling with finger
   document.getElementById("eventTitle").textContent = "touchstart";
+  document.getElementById("altKey").textContent = evt.altKey;
+  document.getElementById("ctrlKey").textContent = evt.ctrlKey;
+  document.getElementById("metaKey").textContent = evt.metaKey;
+  document.getElementById("shiftKey").textContent = evt.shiftKey;
 }
 
 dut.ontouchend = function(evt) {
-  evt.preventDefault();
+  evt.preventDefault(); // prevent screen scrolling with finger
   document.getElementById("eventTitle").textContent = "touchend";
 }
 
 dut.ontouchmove = function(evt) {
-  evt.preventDefault();
+  evt.preventDefault(); // prevent screen scrolling with finger
   document.getElementById("eventTitle").textContent = "touchmove";
 }
 
 dut.ontouchcancel = function(evt) {
-  evt.preventDefault();
+  evt.preventDefault(); // prevent screen scrolling with finger
   document.getElementById("eventTitle").textContent = "touchcancel";
+}
+
+dut.onmousedown = function(evt) {
+  dut.dispatchEvent(new Event("touchstart"));
 }
