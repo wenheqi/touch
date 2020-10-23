b16d777ad5441ecb0385c0b7a62dd9cb3e7dde11
wenheqi <wenhe.qi@outlook.com>
2020-10-22 18:21:15 -0700

    Update touch.js

--- touch.js
+++ touch.js
@@ -8,6 +8,21 @@ dut.ontouchstart = function(evt) {
   document.getElementById("ctrlKey").textContent = evt.ctrlKey;
   document.getElementById("metaKey").textContent = evt.metaKey;
   document.getElementById("shiftKey").textContent = evt.shiftKey;
+  document.getElementById("targetTouchesLength").textContent = evt.targetTouches.length;
+  if (evt.targetTouches.length > 0) {
+    document.getElementById("tIdentifier").textContent = evt.identifier;
+    document.getElementById("tScreenX").textContent = evt.screenX;
+    document.getElementById("tScreenY").textContent = evt.screenY;
+    document.getElementById("tClientX").textContent = evt.clientX;
+    document.getElementById("tClientY").textContent = evt.clientY;
+    document.getElementById("tPageX").textContent = evt.pageX;
+    document.getElementById("tPageY").textContent = evt.pageY;
+    document.getElementById("tTarget").textContent = evt.target.tag + "#" + evt.target.id;
+    document.getElementById("tRadiusX").textContent = evt.radiusX;
+    document.getElementById("tRadiusY").textContent = evt.radiusY;
+    document.getElementById("tRotationAngle").textContent = evt.rotationAngle;
+    document.getElementById("tForce").textContent = evt.force;
+  }
 }
 
 dut.ontouchend = function(evt) {
