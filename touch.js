7b07f8d7ffebf2474212b8ce2829ee9703611f9e
wenheqi <wenhe.qi@outlook.com>
2020-10-22 17:33:32 -0700

    Update touch.js

--- touch.js
+++ touch.js
@@ -2,17 +2,21 @@
 const dut = document.getElementById("testdiv");
 
 dut.ontouchstart = function(evt) {
+  evt.preventDefault();
   document.getElementById("eventTitle").textContent = "touchstart";
 }
 
 dut.ontouchend = function(evt) {
+  evt.preventDefault();
   document.getElementById("eventTitle").textContent = "touchend";
 }
 
 dut.ontouchmove = function(evt) {
+  evt.preventDefault();
   document.getElementById("eventTitle").textContent = "touchmove";
 }
 
 dut.ontouchcancel = function(evt) {
+  evt.preventDefault();
   document.getElementById("eventTitle").textContent = "touchcancel";
 }
